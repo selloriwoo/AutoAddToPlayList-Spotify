5af2097538245b772b28f4efd5fd4f25b2d853bc
selloriwoo <sellossier@gmail.com>
2025-08-09 01:02:09 +0900

    feature: 노래 제목으로 스포티파이 노래 url 불러오기

--- spotify.py
+++ spotify.py
@@ -1,6 +1,9 @@
 import requests
 import os
 import dotenv
+import ast
+
+import CrawlingKaraoke
 
 dotenv.load_dotenv()
 
@@ -33,7 +36,22 @@ headers = {
     "Authorization": f"Bearer {access_token}"
 }
 
-url = "https://api.spotify.com/v1/artists/2DaxqgrOhkeH0fpeiQq2f4?si=V9dcndhEQG6U8s93jTuuKA"
-response = requests.get(url, headers=headers)
-artist_info = response.json()
-print(artist_info)
+#아티스트 검색
+# url = "https://api.spotify.com/v1/artists/2DaxqgrOhkeH0fpeiQq2f4?si=V9dcndhEQG6U8s93jTuuKA"
+# response = requests.get(url, headers=headers)
+# artist_info = response.json()
+# print(artist_info)
+
+#곡 검색
+# url = "https://api.spotify.com/v1/search?q=%E3%82%B5%E3%83%A0%E3%83%A9%E3%82%A4%E3%83%8F%E3%83%BC%E3%83%88&type=track&market=JP&limit=1"
+# response = requests.get(url, headers=headers)
+# print(response.json().get("tracks").get("items")[0].get("external_urls").get("spotify"))
+# print(CrawlingKaraoke.resultTitle)
+
+result_spotify_song_url= []
+for item in CrawlingKaraoke.resultTitle:
+    url = f"https://api.spotify.com/v1/search?q={item}&type=track&market=JP&limit=1"
+    response = requests.get(url, headers=headers)
+    result_spotify_song_url.append(response.json().get("tracks").get("items")[0].get("external_urls").get("spotify"))
+
+print(result_spotify_song_url)
